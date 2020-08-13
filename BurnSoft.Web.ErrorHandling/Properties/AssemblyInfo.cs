c412de0f3997155e51ba1195747f0969e7a5c99d
Joe M <joe.mireles@burnsoft.net>
2020-08-13 08:03:39 -0400

    Updated description

--- BurnSoft.Web.ErrorHandling/Properties/AssemblyInfo.cs
+++ BurnSoft.Web.ErrorHandling/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnSoft.Web.ErrorHandling")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple Library that can help send error messages from your website to you or your support email about any application exception errors that have occurred in a pretty HTML report")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Web.ErrorHandling")]
